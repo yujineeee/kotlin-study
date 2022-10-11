22c84b918f48871624cc0ae0a178005a5bbfa658
장유진/전시개발팀 <yujin.jang@musinsa.com>
2022-10-11 22:51:50 +0900

    lecture06 - 코틀린에서 반복문을 다루는 방법

---
+++ src/lecture06/java/Lecture06Main.java
@@ -0,0 +1,17 @@
+package lecture06.java;
+
+public class Lecture06Main {
+
+    public static void main(String[] args) {
+
+        List<Long> numbers = java.util.Arrays.asList(1L, 2L, 3L);
+        for (long number : numbers) {
+            System.out.println(number);
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
