125633315dc74e40b868688cb5dd533b4810ac2b
장유진/전시개발팀 <yujin.jang@musinsa.com>
2022-10-11 22:37:11 +0900

    lecture05 - 코틀린에서 조건문을 다루는 방법

---
+++ src/lecture05/java/Lecture05Main.java
@@ -0,0 +1,48 @@
+package lecture05.java;
+
+public class Lecture05Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    private void validateScoreIsNotNegative(int score) {
+        if (score < 0) {
+            throw new IllegalArgumentException(String.format("%s는 0보다 작을 수 없습니다.", score));
+        }
+    }
+
+    private String getPassOrFail(int score) {
+        if (score >= 50) {
+            return "P";
+        } else {
+            return "F";
+        }
+    }
+
+    private String getGrade(int score) {
+        if (score >= 90) {
+            return "A";
+        } else if (score >= 80){
+            return "B";
+        } else if (score >= 70){
+            return "C";
+        } else {
+            return "D";
+        }
+    }
+
+    private String getGradeWithSwitch(int score) {
+        switch (score / 10) {
+            case 9:
+                return "A";
+            case 8:
+                return "B";
+            case 7:
+                return "C";
+            default:
+                return "D";
+        }
+    }
+}
